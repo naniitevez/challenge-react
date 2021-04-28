d700f82eb3c283313152dfd4facdb8ad7214dfd1
Nicole Tevez <71675833+naniitevez@users.noreply.github.com>
2021-04-27 21:35:00 -0300

    Renderizado condicional

--- challenge/src/pages/Login/Login.jsx
+++ challenge/src/pages/Login/Login.jsx
@@ -1,8 +1,12 @@
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "./Login.css";
 import InputComponent from "./components/InputComponent";
+import { useState } from "react";
 
 const Login = () => {
+  const [inputEmail, setInputEmail] = useState("")
+  const [inputPassword, setInputPassword] = useState("")
+
   return (
     <>
       <Container>
@@ -15,12 +19,16 @@ const Login = () => {
                   label={"Email"}
                   type={"email"}
                   placeholder={"Ingresa tu dirección email"}
+                  state={inputEmail}
+                  setState={setInputEmail}
                 />
                 <InputComponent
                   idControl={"formBasicPassword"}
                   label={"Contraseña"}
                   type={"password"}
                   placeholder={"Ingresa tu contraseña"}
+                  state={inputPassword}
+                  setState={setInputPassword}
                 />
                 <Button className="form__button" variant="light" type="submit">
                   Ingresar
--- challenge/src/pages/Login/components/InputComponent.jsx
+++ challenge/src/pages/Login/components/InputComponent.jsx
@@ -1,15 +1,52 @@
-import Form from 'react-bootstrap/Form'
+import Form from "react-bootstrap/Form";
 
-const InputComponent = ({idControl, label, type, placeholder}) => {
+const InputComponent = ({
+  state,
+  setState,
+  estado,
+  setEstado,
+  idControl,
+  label,
+  type,
+  placeholder,
+}) => {
+  let valid = true;
+
+  const validations = () => {
+    if (state === "") {
+      valid = false
+    } else {
+      valid = true
+    }
+  }
+
+  const inputValidation = (e) => {
+    setState(e.target.value)
+  }
+
+
   return (
-    <Form.Group controlId={idControl}>
-      <Form.Label>{label}</Form.Label>
-      <Form.Control
-        className="form__input"
-        type={type}
-        placeholder={placeholder}
-      />
-    </Form.Group>
+    <>
+      <Form.Group controlId={idControl}>
+        <Form.Label>{label}</Form.Label>
+        <Form.Control
+          className="form__input"
+          type={type}
+          placeholder={placeholder}
+          value={state}
+          onChange={inputValidation}
+          onKeyUp={() => console.log("on KEY UP")}
+          onBlur={() => console.log("on BLUR")}
+        />
+      {!valid && (
+        <div>
+          <Form.Text className="text-muted">
+            El campo de {label} no puede estar vacío.
+          </Form.Text>
+        </div>
+      )}
+      </Form.Group>
+    </>
   );
 };
 
