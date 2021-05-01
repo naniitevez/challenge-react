edae43881a47d38f50eab93f68d2118149db137f
Nicole Tevez <71675833+naniitevez@users.noreply.github.com>
2021-05-01 16:45:08 -0300

    Home listo

--- challenge/src/pages/Home/Home.jsx
+++ challenge/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import MyNavbar from "./components/MyNavbar";
-import { Container, Row, Col } from "react-bootstrap";
-import MyCards from "./components/MyCards";
+import { Container } from "react-bootstrap";
 import { useContext } from "react";
 import HeroesContext from "../../context/superheros/Index";
+import MyCard from "./components/MyCard";
 
 const Home = () => {
-  const { heroes } = useContext(HeroesContext);
+  const { heroes, token } = useContext(HeroesContext);
 
   return (
     <>
@@ -13,22 +13,9 @@ const Home = () => {
       <button onClick={() => console.log(heroes)}>CliCK</button>
       <Container>
         <div className="row__container">
-            {heroes.map((hero, index) => <MyCards key={index} {...hero} />)}
-            {/* <Col xs={12} md={6} lg={4}>
-              <MyCards />
-            </Col>
-            <Col xs={12} md={6} lg={4}>
-              <MyCards />
-            </Col>
-            <Col xs={12} md={6} lg={4}>
-              <MyCards />
-            </Col>
-            <Col xs={12} md={6} lg={4}>
-              <MyCards />
-            </Col>
-            <Col xs={12} md={6} lg={4}>
-              <MyCards />
-            </Col> */}
+            {
+              heroes.map((hero, index) => <MyCard key={index} {...hero} token={token} />)
+            }
         </div>
       </Container>
     </>
--- challenge/src/routes/Routes.js
+++ challenge/src/routes/Routes.js
@@ -1,13 +1,16 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import HeroDetail from '../pages/HeroeDetail/HeroDetail';
 import Home from '../pages/Home/Home';
 import Login from '../pages/Login/Login';
 
 const Routes = () => {
+
     return (
         <Router>
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/home" component={Home}/>
+                <Route path="/heroes/:id" component={HeroDetail}/>
             </Switch>
         </Router>
     );
